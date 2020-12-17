40e9488ae0efbdd9018699a2baad86013fc27329
Grace <gchen20@stuy.edu>
2020-12-16 22:38:16 -0500

    Tested more cases (tried to make a random test case generator but wasn't able to correctly get it to work), tested with a tester from hwdiscussion, and verfied my code

--- Tester.java
+++ Tester.java
@@ -12,9 +12,13 @@ public class Tester {
   public static void main(String[]args){
     int[] array = {100, 2, 5, 10};
     int[] array2 = {5,2,-12,5,16};
-    int[] array3 = {1,2,3,4,5};
+    int[] array3 = {100, 65, 1,2,3,4,5, -300};
+    int[] array4 = {6,1,2,3,4,5};
+    int[] array5 = {1,2,3,4,5};
     Sorts.bubbleSort(array2);
     Sorts.bubbleSort(array);
     Sorts.bubbleSort(array3);
+    Sorts.bubbleSort(array4);
+    Sorts.bubbleSort(array5);
   }
 }
