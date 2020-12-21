ad18bfa2df742da835a14e9041d669fec711f260
Grace <gchen20@stuy.edu>
2020-12-21 01:28:52 -0500

    Created another tester to test run time

---
+++ Driver.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+public class Driver{
+
+  public static void main(String[]args){
+    //args 0 must be size of the Array
+    //args 1 must be "bubble" or "test" (unless you add more)
+    int[] randData = new int[Integer.parseInt(args[0])];
+
+    for(int i = 0 ; i < randData.length; i++){
+      randData[i] =(int)(Math.random()*10000);
+    }
+
+    if(args[1].equals("bubble")){
+      Sorts.bubbleSort(randData);
+    }
+    if(args[1].equals("bubbleTest")){
+      int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+      Arrays.sort(randData);
+      Sorts.bubbleSort(randDataBubble);
+
+
+      if(Arrays.equals(randData,randDataBubble)){
+        System.out.println("Bubble Correct!");
+      }
+      else{
+        System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
+      }
+    }
+
+    if(args[1].equals("selectionTest")){
+      int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+      Arrays.sort(randData);
+      Sorts.selectionSort(randDataBubble);
+
+
+      if(Arrays.equals(randData,randDataBubble)){
+        System.out.println("Selection Correct!");
+      }
+      else{
+        System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
+      }
+    }
+
+    if(args[1].equals("insertionTest")){
+      int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+      Arrays.sort(randData);
+      Sorts.insertionSort(randDataBubble);
+
+
+      if(Arrays.equals(randData,randDataBubble)){
+        System.out.println("Insertion Correct!");
+      }
+      else{
+        System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
+      }
+    }
+
+  }
+
+}
